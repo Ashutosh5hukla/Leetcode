4102bce9e5c07eb5ca696b7e296b28bb8e6c2052
Ashutosh5hukla <ashutosh20051978@gmail.com>
2025-11-11 07:40:04 +0530

    Time: 4 ms (7.32%), Space: 43.4 MB (5.58%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,27 +1,25 @@
-class Solution {
-    public int specialArray(int[] arr) {
-        int s=0;
-        int e=arr.length;
-        int x=-1;
+import java.util.*;
+class Solution{
+    public int specialArray(int[] arr){
+        Arrays.sort(arr);
+        int n=arr.length,s=0,e=n,x=-1;
         while(s<=e){
             int m=s+(e-s)/2;
-            System.out.print(m+",");
-            int ans=s(arr,m);
-            if(ans==m){
-                x=m;
-                s=m+1;
-                
-            }
-            else if(ans>m)s=m+1;
+            int l=lb(arr,m);
+            int count=n-l;
+            if(count==m){x=m;s=m+1;}
+            else if(count>m)s=m+1;
             else e=m-1;
         }
-        return x;
+    return x;
     }
-    int s(int arr[],int x){
-        int c=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>=x)c++;
+    private int lb(int[] arr,int x){
+        int l=0,r=arr.length;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]>=x)r=mid;
+            else l=mid+1;
         }
-        return c;
+        return l;
     }
 }
