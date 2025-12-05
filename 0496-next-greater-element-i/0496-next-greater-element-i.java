e5a0e7ba1cbc2555a0a211a26dfc27c743cd8e0b
Ashutosh5hukla <ashutosh20051978@gmail.com>
2025-12-05 16:38:03 +0530

    Time: 3 ms (88.38%), Space: 45.5 MB (9.42%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,16 +1,18 @@
 class Solution {
-    public int[] nextGreaterElement(int[] arr, int[] brr){
-        Stack<Integer> st=new Stack<>();
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int freq[]=new int[10001];
-        for(int i=brr.length-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<=brr[i])st.pop();
-            if(st.isEmpty())freq[brr[i]]=-1;
-            else freq[brr[i]]=st.peek();
-            st.push(brr[i]);
+        Stack<Integer> st=new Stack<>();
+        Arrays.fill(freq,-1);
+        int j=nums2.length-1;
+        while(j>=0){
+            while(!st.isEmpty() && st.peek() <=nums2[j])st.pop();
+            if(!st.isEmpty())freq[nums2[j]]=st.peek();
+            st.push(nums2[j]);
+            j--;
         }
-        int ans[]=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            ans[i]=freq[arr[i]];
+        int ans[]=new int[nums1.length];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=freq[nums1[i]];
         }
         return ans;
     }
