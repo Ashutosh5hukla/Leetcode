9bfdb5b386665d90bce6b6530256fd9d30f6f249
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-01-12 19:50:12 +0530

    Time: 0 ms (100%), Space: 45.4 MB (6.46%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean search(int[] arr, int t) {
+         int s=0;
+        int e=arr.length-1;
+        while(s<e){
+            int m=s+(e-s)/2;
+            if(arr[m]==t)return true;
+            if (arr[s] == arr[m] && arr[e] == arr[m]) {
+            s++;
+            e--;
+        }else if(arr[s]<=arr[m]){
+                if(t>=arr[s] && t<arr[m]){
+                    e=m-1;
+                }else{
+                    s=m+1;
+                }
+        }else{
+            if(t>arr[m] && t<=arr[e]){
+                    s=m+1;
+            }else{
+                e=m-1;
+            }
+        }
+        }
+        return arr[s]==t;
+    }
+}
+
+// class Solution {
+//     public boolean search(int[] arr, int t) {
+//     int s = 0, e = arr.length - 1;
+//     while (s <= e) {
+//         int m = s + (e - s) / 2;
+//         if (arr[m] == t) return true;
+//         if (arr[s] == arr[m] && arr[e] == arr[m]) {
+//             s++;
+//             e--;
+//         } else if (arr[s] <= arr[m]) {
+//             if (t >= arr[s] && t < arr[m]) {
+//                 e = m - 1;
+//             } else {
+//                 s = m + 1;
+//             }
+//         } else {
+//             if (t > arr[m] && t <= arr[e]) {
+//                 s = m + 1;
+//             } else {
+//                 e = m - 1;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// }
