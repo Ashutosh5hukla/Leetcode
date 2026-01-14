374ac57060215d920bfecd17d444029ed96c2659
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-01-14 18:37:20 +0530

    Time: 13 ms (25.51%), Space: 80.6 MB (19.73%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -0,0 +1,41 @@
+class Solution {
+    int min(int arr[]){
+        int m=Integer.MAX_VALUE;
+        for(int ele:arr)m=Math.min(m,ele);
+        return m;
+    }
+    int max(int arr[]){
+        int m=Integer.MIN_VALUE;
+        for(int ele:arr)m=Math.max(m,ele);
+        return m;
+    }
+    public int minimizeArrayValue(int[] arr) {
+        int s=Math.max(arr[0],min(arr));
+        int e=max(arr);
+        int ans=-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(valid(m,arr)){
+                ans=m;
+                e=m-1;
+            }
+            else s=m+1;
+        }
+        return ans;
+    }
+    boolean valid(int m,int arr[]){
+        if(m<arr[0])return false;
+        long req=m-arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>m){
+                long curr=arr[i]-m;
+                if(curr>req)return false;
+                else req-=curr;
+            }else{
+                req+=(m-arr[i]);
+            }
+        }
+        return true;
+    }
+
+}
