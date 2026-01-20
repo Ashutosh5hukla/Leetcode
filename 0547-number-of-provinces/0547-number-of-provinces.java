768921e008ff8e65a31f6fbb25cff182ef491a52
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-01-20 22:07:23 +0530

    Time: 1 ms (86.79%), Space: 47.2 MB (67.11%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -4,24 +4,19 @@ class Solution {
         int c=0;
         for(int i=0;i<adj.length;i++){
             if(!vis[i]){
-                bfs(adj,vis,i);
+                dfs(adj,vis,i);
                 c++;
             }
         }
         return c;
     }
-    public void bfs(int[][] adj,boolean vis[],int i){
+    public void dfs(int[][] adj,boolean vis[],int i){
         vis[i]=true;
-        Queue<Integer> q=new LinkedList<>();
-        q.add(i);
-        while(!q.isEmpty()){
-            int f=q.remove();
-            for(int j=0;j<adj.length;j++){
-                if(adj[f][j]==1 && !vis[j]){
-                    q.add(j);
-                    vis[j]=true;
-                }
+        for(int j=0;j<adj.length;j++){
+            if(adj[i][j]==1 && !vis[j]){
+                dfs(adj,vis,j);
             }
         }
+        
     }
 }
