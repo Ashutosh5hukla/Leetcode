ff87b2b091da1d140e271391fd6b9867d59f2a46
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-01-22 20:37:18 +0530

    Time: 1 ms (44.46%), Space: 47 MB (33.32%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] floodFill(int[][] adj, int sr, int sc, int color) {
         boolean vis[][]=new boolean[adj.length][adj[0].length];
-        bfs(adj,vis,sr,sc,adj[sr][sc]);
+        dfs(adj,vis,sr,sc,adj[sr][sc]);
         for(int i=0;i<adj.length;i++){
             for(int j=0;j<adj[i].length;j++){
                 if(vis[i][j])adj[i][j]=color;
@@ -17,31 +17,20 @@ class Solution {
             this.sc=sc;
         }
     }
-    void bfs(int [][] adj,boolean vis[][],int sr,int sc,int col){
-        vis[sr][sc]=true;
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(sr,sc));
-        while(!q.isEmpty()){
-            Pair t=q.remove();
-            int r=t.sr;
-            int c=t.sc;
+    void dfs(int [][] adj,boolean vis[][],int r,int c,int col){
+        vis[r][c]=true;
             if(r>0 && adj[r-1][c]==col && vis[r-1][c]==false){
-                q.add(new Pair(r-1,c));
-                vis[r-1][c]=true;
+                dfs(adj,vis,r-1,c,col);
             }
             if(c>0 && adj[r][c-1]==col && vis[r][c-1]==false){
-                q.add(new Pair(r,c-1));
-                vis[r][c-1]=true;
+                dfs(adj,vis,r,c-1,col);
             }
             if(r<adj.length-1 && adj[r+1][c]==col && vis[r+1][c]==false){
-                q.add(new Pair(r+1,c));
-                vis[r+1][c]=true;
+                dfs(adj,vis,r+1,c,col);
             }
             if(c<adj[0].length-1 && adj[r][c+1]==col && vis[r][c+1]==false){
-                q.add(new Pair(r,c+1));
-                vis[r][c+1]=true;
+                dfs(adj,vis,r,c+1,col);
             }
-        }
 
     }
 }
