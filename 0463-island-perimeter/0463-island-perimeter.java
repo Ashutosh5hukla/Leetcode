fd28e1da1df96fc24aa453c563e84fa40e745190
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-02-03 17:05:43 +0530

    Time: 9 ms (13.34%), Space: 47.9 MB (5.42%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,21 +1,59 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int ans = 0;
-
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(grid[i][j] == 1){
-                    ans += 4;
-                    if(i > 0 && grid[i-1][j] == 1){
-                        ans -= 2;
-                    }
-                    if(j > 0 && grid[i][j-1] == 1){
-                        ans -= 2;
-                    }
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean vis[][]=new boolean[m][n];
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    return bfs(grid,vis,i,j);
                 }
             }
+        }
+        return 0;
+    }
+    int bfs(int[][]grid,boolean[][] vis,int r,int c){
+        int m=grid.length;
+        int n=grid[0].length;
+        int ans=0;
+        vis[r][c]=true;
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int[]{r,c});
+        while(!q.isEmpty()){
+            int a=4;
+            int arr[]=q.poll();
+            int row=arr[0];
+            int col=arr[1];
+            if(row>0 && grid[row-1][col]==1){
+                a--;
+                if(!vis[row-1][col]){
+                    q.add(new int[]{row-1,col});
+                    vis[row-1][col]=true;
+                }
+            }
+            if(row<m-1 && grid[row+1][col]==1){
+                a--;
+                if(!vis[row+1][col]){
+                    q.add(new int[]{row+1,col});
+                    vis[row+1][col]=true;
+                }
+            }
+            if(col>0 && grid[row][col-1]==1){
+                a--;
+                if(!vis[row][col-1]){
+                    q.add(new int[]{row,col-1});
+                    vis[row][col-1]=true;
+                }
+            }
+            if(col<n-1 && grid[row][col+1]==1){
+                a--;
+                if(!vis[row][col+1]){
+                    q.add(new int[]{row,col+1});
+                    vis[row][col+1]=true;
+                }
+            }
+            ans+=a;
         }
         return ans;
     }
