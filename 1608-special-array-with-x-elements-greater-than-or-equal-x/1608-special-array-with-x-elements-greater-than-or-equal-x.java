82819215285d9002a19d6bf3624162cf3b8f02a3
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-03-23 20:48:46 +0530

    Time: 0 ms (100%), Space: 43.2 MB (56.81%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,25 +1,19 @@
 class Solution {
     public int specialArray(int[] nums) {
-        int s=0;
-        int e=nums.length;
-        Arrays.sort(nums);
-        while(s<=e){
-            int m=s+(e-s)/2;
-            if((nums.length-lb(m,nums))==m)return m;
-            if((nums.length-lb(m,nums))<m)e=m-1;
-            else s=m+1;
+        int n = nums.length;
+        int[] freq=new int[n+1];
+        for (int ele : nums) {
+            if (ele >= n)freq[n]++;
+            else freq[ele]++;
+        }
+        int[] cs=new int[n+1];// cumulative sum
+        cs[n]=freq[n];
+        for(int i=n-1;i>=0;i--) {
+            cs[i]=cs[i+1]+freq[i];
+        }
+        for(int i=0;i<=n;i++) {
+            if(cs[i]==i)return i;
         }
         return -1;
-    }
-    int lb(int x,int arr[]){
-        int s=0;
-        int e=arr.length-1;
-        while(s<=e){
-            int m=s+(e-s)/2;
-            if(arr[m]>=x){
-                e=m-1;
-            }else s=m+1;
-        }
-        return s;
     }
 }
