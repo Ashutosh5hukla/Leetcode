1e90f76eeec616824c1e1a5d61575aed3424a258
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-03-25 23:15:12 +0530

    Time: 25 ms (34.93%), Space: 48.1 MB (84.41%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -9,35 +9,17 @@ class Solution {
         }
         // sort by difficulty
         list.sort((a, b) -> a[0] - b[0]);
-        // prefix max profit
-        int mx = list.get(0)[1];
-        for (int i = 1; i < list.size(); i++) {
-            mx = Math.max(mx, list.get(i)[1]);
-            list.get(i)[1] = mx;
-        }
-        int ans = 0;
-        // for each worker → binary search
-        for (int w : worker) {
-            int idx = binarySearch(list, w);
-            if (idx != -1) {
-                ans += list.get(idx)[1];
+        Arrays.sort(worker);
+        int mp=0;
+        int res=0;
+        int j=0;
+        for(int i=0;i<worker.length;i++){
+            while(j<list.size() && list.get(j)[0]<=worker[i]){
+                mp=Math.max(mp,list.get(j)[1]);
+                j++;
             }
+            res+=mp;
         }
-        return ans;
-    }
-    // find last index where difficulty <= worker ability
-    int binarySearch(ArrayList<int[]> list, int target) {
-        int s = 0, e = list.size() - 1;
-        int ans = -1;
-        while (s <= e) {
-            int m = s + (e - s) / 2;
-            if (list.get(m)[0] <= target) {
-                ans = m;
-                s = m + 1;
-            } else {
-                e = m - 1;
-            }
-        }
-        return ans;
+        return res;
     }
 }
