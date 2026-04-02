2b8f60e511edb038691077986bc010e324bab4b6
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-04-03 00:33:40 +0530

    Time: 19 ms (44.24%), Space: 76.1 MB (84.65%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int minCapability(int[] arr, int k) {
+        int s = min(arr);
+        int e = mx(arr);
+        int res = e;
+
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+
+            if (check(arr, k, m)) {
+                res = m;
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        return res; // 🔥 missing
+    }
+
+    boolean check(int[] arr, int k, int m) {
+        int housecnt = 0;
+        int i = 0;
+
+        while (i < arr.length) {
+            if (arr[i] <= m) {
+                housecnt++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        return housecnt >= k;
+    }
+
+    int min(int[] arr) {
+        int mn = Integer.MAX_VALUE;
+        for (int x : arr) mn = Math.min(mn, x);
+        return mn;
+    }
+
+    int mx(int[] arr) {
+        int mx = Integer.MIN_VALUE;
+        for (int x : arr) mx = Math.max(mx, x);
+        return mx;
+    }
+}
