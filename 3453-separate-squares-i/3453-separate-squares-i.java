dfcf9ccb11532b102821422ef3aadcdc35a5363f
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-04-06 11:50:22 +0530

    Time: 89 ms (92.72%), Space: 149 MB (31.62%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.java
+++ 3453-separate-squares-i/3453-separate-squares-i.java
@@ -1,43 +1,54 @@
 class Solution {
-    private int[][] squares;
 
-    public double separateSquares(int[][] squares) {
-        this.squares = squares;
+    private boolean check(int[][] squares, double midY, double total) {
+        double bottomArea = 0.0;
 
-        double totalArea = 0;
-        double right = 0;
         for (int[] square : squares) {
-            totalArea += (1.0 * square[2] * square[2]);
-            right = Math.max(right, square[1] + square[2]);
-        }
+            double y = square[1];
+            double l = square[2];
 
-        double left = 0;
-        double buffer = 1e-5;
+            double bottomY = y;
+            double topY = y + l;
 
-        while (Math.abs(right - left) > buffer) {
-            double mid = (left + right) / 2;
-            double lowerArea = findArea(mid);
-            if (lowerArea * 2 >= totalArea) {
-                right = mid;                
-            } else {
-                left = mid;
+            if (midY >= topY) {
+                // full square below
+                bottomArea += l * l;
+            } else if (midY > bottomY) {
+                // partial square below
+                bottomArea += (midY - bottomY) * l;
             }
         }
 
-        return right;
+        return bottomArea >= total / 2.0; //Is bottom area more than above ?
     }
 
-    private double findArea(double y) {
-        double area = 0;
+    public double separateSquares(int[][] squares) {
+        double low = Double.MAX_VALUE;
+        double high = -Double.MAX_VALUE;
+        double total = 0.0;
+
         for (int[] square : squares) {
-            int y1 = square[1], y2 = square[1] + square[2];
-            if (y > y2) {
-                area += (1.0 * square[2] * square[2]);
-            } else if (y1 < y && y <= y2) {
-                area += (1.0 * (y - y1) * square[2]);
-            }
+            double y = square[1];
+            double l = square[2];
+
+            total += l * l;
+            low = Math.min(low, y);
+            high = Math.max(high, y + l);
         }
 
-        return area;
+        double resultY = 0.0;
+
+        while (high - low > 1e-5) {
+            double midY = low + (high - low) / 2.0;
+            resultY = midY;
+
+            if (check(squares, midY, total)) {
+                // bottom area is more than half, move down
+                high = midY;
+            } else {
+                low = midY;
+            }
+        }
+        return resultY;
     }
 }
