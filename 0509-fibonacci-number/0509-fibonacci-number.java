190e837597d36db7da077e7ce789f1ec59924b47
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-04-07 23:52:51 +0530

    Time: 0 ms (100%), Space: 41.9 MB (76.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n==1 || n==0)return n;
-        return fib(n-1)+fib(n-2);
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        if(n==0 || n==1)return n;
+        dp[0]=0;
+        dp[1]=1;
+
+        return nemo(n,dp);
+        // return dp[dp.length-1];
+    }
+    int nemo(int n,int arr[]){
+        if(arr[n]!=-1)return arr[n];
+        return arr[n]=nemo(n-1,arr)+nemo(n-2,arr);    
     }
 }
