4c3f2627b89bb8ea53fce6d80b0092a474a5cce8
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-04-08 21:27:13 +0530

    Time: 0 ms (100%), Space: 42.2 MB (43.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
         if(n<=3)return n;
-        int dp[]=new int[n+1];
-        dp[1]=1;
-        dp[2]=2;
+        int dp[]=new int[3];
+        dp[0]=1;
+        dp[1]=2;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            dp[2]=dp[0]+dp[1];
+            dp[0]=dp[1];
+            dp[1]=dp[2];
         }
-        return dp[n];
+        return dp[2];
     }   
 }
