6500618797895f76dda56f510226441f521f0674
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-04-13 20:36:07 +0530

    Time: 62 ms (30.88%), Space: 122.1 MB (27.19%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,20 +1,21 @@
 class Solution {
-    public long maxAlternatingSum(int[] nums) {
-        long dp[][]=new long[nums.length][2];
-        for(int i=0;i<dp.length;i++){
+    public long maxAlternatingSum(int[] arr) {
+        int n=arr.length;
+        long dp[][]=new long[n][2];
+        for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        long ans=helper(nums,0,1,dp);//1==iseven
+        long ans=helper(arr,0,1,dp);
         return ans;
     }
-    long helper(int arr[],int i,int isEven,long dp[][]){
-        if(i>=arr.length)return 0;
+    long helper(int[]arr,int i,int isEven,long[][] dp){
+        if(i>=dp.length)return 0;
         if(dp[i][isEven]!=-1)return dp[i][isEven];
         long s=helper(arr,i+1,isEven,dp);
         long t;
-        if(isEven==1){
+        if(isEven==1){//even idx so add
             t=arr[i]+helper(arr,i+1,0,dp);
-        }else{
+        }else{//odd so sub
             t=helper(arr,i+1,1,dp)-arr[i];
         }
         return dp[i][isEven]=Math.max(t,s);
