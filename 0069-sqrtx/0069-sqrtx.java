efbf8bfda32d417f306ad9de7329f1d8297badd8
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-05-13 06:14:32 +0530

    Time: 1 ms (97.92%), Space: 42.4 MB (77.24%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        long s=0;
+        long e=x;
+        while(s<=e){
+            long m=s+(e-s)/2;
+            if(m*m==x) return (int)m;
+            else if(m*m>(long)x)e=m-1;
+            else s=m+1;
+        }
+        return (int)e;
+    }
+}
