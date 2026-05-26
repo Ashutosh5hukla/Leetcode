92dfb3ccd720bcb3f963afdff54a69dd7453e720
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-05-26 23:19:11 +0530

    Time: 0 ms (100%), Space: 43.3 MB (76.9%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] plusOne(int[] arr) {
-       
        for(int i=arr.length-1;i>=0;i--){
             if(arr[i]<9){
                 arr[i]++;
@@ -11,6 +10,5 @@ class Solution {
     int[] ans=new int[arr.length+1];
     ans[0]=1;
     return ans;
-
     }
 }
