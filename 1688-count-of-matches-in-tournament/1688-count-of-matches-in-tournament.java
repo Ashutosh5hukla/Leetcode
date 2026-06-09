2928bfb122c882cb605ef279d56e525476d8a114
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-06-09 15:21:20 +0530

    Time: 0 ms (100%), Space: 42.1 MB (74.08%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int mp = 0;
+        while (n > 1) {
+            if (n % 2 == 0) {
+                mp += n / 2;
+                n /= 2;
+            } else {
+                mp += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+        return mp;
+    }
+}
