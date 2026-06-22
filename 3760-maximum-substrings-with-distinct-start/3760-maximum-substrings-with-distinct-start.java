6879cf076c5d01e42c8ae6ddc7f26c5a9c2a7867
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-06-22 10:00:50 +0530

    Time: 212 ms (5.01%), Space: 47.8 MB (56.34%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -1,15 +1,5 @@
 class Solution {
     public int maxDistinct(String s) {
-        int freq[]=new int[26];
-        int ans=0;
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            freq[(ch-'a')]=1;
-        }
-        for(int i=0;i<26;i++){
-            if(freq[i]==1)ans++;
-        }
-        return ans;
-
+       return (int)s.chars().distinct().count();
     }
 }
