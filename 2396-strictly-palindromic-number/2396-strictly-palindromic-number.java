d76594f9d8b46fec4eed31c864ac55fd8a9c536d
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-06-22 13:19:54 +0530

    Time: 0 ms (100%), Space: 41.6 MB (95.73%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        for(int b=2;b<=n-2;b++){
-            String s=convert(n,b);
-            String rev=new StringBuilder(s).reverse().toString();
-            if(!s.equals(rev)) return false;
-        }
-        return true;
+        // for(int b=2;b<=n-2;b++){
+        //     String s=convert(n,b);
+        //     String rev=new StringBuilder(s).reverse().toString();
+        //     if(!s.equals(rev)) return false;
+        // }
+        // return true;
+        return false;
     }
     private String convert(int n,int base){
         StringBuilder sb=new StringBuilder();
