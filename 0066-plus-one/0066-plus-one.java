1680017bdbba5627d8a4a02ab1e9fbb56fa91dbe
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-06-29 16:48:16 +0530

    Time: 1 ms (3.88%), Space: 43.6 MB (23.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,14 +1,27 @@
 class Solution {
-    public int[] plusOne(int[] arr) {
-       for(int i=arr.length-1;i>=0;i--){
-            if(arr[i]<9){
-                arr[i]++;
-                return arr;
+    public int[] plusOne(int[] d) {
+        ArrayList<Integer> ls=new ArrayList<>();
+        for(int i=0;i<d.length;i++){
+            ls.add(d[i]);
             }
-            arr[i]=0;
-       }
-    int[] ans=new int[arr.length+1];
-    ans[0]=1;
-    return ans;
+        Collections.reverse(ls);
+        int c=0;
+        for(int i=0;i<ls.size();i++){
+            if(ls.get(i)==9){
+                ls.set(i,0);
+                c=1;
+            }else{
+                if(c==1)c=0;
+                ls.set(i,ls.get(i)+1);
+                break;
+            }
+        }
+        if(c==1)ls.add(1);
+        Collections.reverse(ls);
+        int ans[]=new int[ls.size()];
+        for(int i=0;i<ls.size();i++){
+            ans[i]=ls.get(i);
+        }
+        return ans;
     }
-}
+}//8999 9998 000
