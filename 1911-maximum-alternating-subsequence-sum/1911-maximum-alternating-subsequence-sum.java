525668d84cf94772b129b04a1330e077463ef422
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-07-20 11:06:52 +0530

    Time: 7 ms (86.7%), Space: 96.1 MB (99.59%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,12 +1,13 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
-        long dp[][]=new long[nums.length][2];
-        dp[0][1]=nums[0];
-        dp[0][0]=0;
+        long even=0;
+        long odd=nums[0];
         for(int i=1;i<nums.length;i++){
-            dp[i][0]=Math.max(dp[i-1][1]-nums[i],dp[i-1][0]);
-            dp[i][1]=Math.max(dp[i-1][0]+nums[i],dp[i-1][1]);
+            long e=Math.max(odd-nums[i],even);
+            long o=Math.max(even+nums[i],odd);
+            even=e;
+            odd=o;
         }
-        return Math.max(dp[nums.length-1][1],dp[nums.length-1][0]);
+        return Math.max(even,odd);
     }
 }
