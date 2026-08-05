cb524bb4119957fb7c306d9ca1f46f7f2c3b40fb
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-08-05 20:42:41 +0530

    Time: 61 ms (23.05%), Space: 56.1 MB (10.63%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -2,29 +2,19 @@ class Solution {
     public int longestStrChain(String[] words) {
         Arrays.sort(words,(a,b)->a.length()-b.length());
         int n=words.length;
-        int dp[][]=new int[n+1][n+1];
+        int[][] dp=new int[n+1][n+1];
         for(int i=n-1;i>=0;i--){
-            for(int p=i-1;p>=-1;p--){
-                if(p==-1 || pred(words[p],words[i])){
-                    dp[i][p+1]=Math.max(dp[i+1][i+1]+1,dp[i+1][p+1]);
-                }else{
-                    dp[i][p+1]=dp[i+1][p+1];
+            for(int prev=i-1;prev>=-1;prev--){
+                int take=0;
+                if(prev==-1 || pred(words[prev],words[i])){
+                    take=1+dp[i+1][i+1];
                 }
+                int skip=dp[i+1][prev+1];
+                dp[i][prev+1]=Math.max(take,skip);
             }
         }
         return dp[0][0];
     }
-    // int helper(String[] w,int i,int prev,int dp[][]){
-    //     if(i==w.length)return 0;
-    //     int t=0;
-    //     if( dp[i][prev+1]!=-1)return dp[i][prev+1];
-        
-    //     if(prev==-1 || pred(w[prev],w[i])){
-    //         t=1+helper(w,i+1,i,dp);
-    //     }
-    //     int s=helper(w,i+1,prev,dp);
-    //     return dp[i][prev+1]=Math.max(t,s);
-    // }
     boolean pred(String wa,String wb){
         if(wb.length()!=wa.length()+1)return false;
         int i=0,j=0;
