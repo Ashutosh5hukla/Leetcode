edd38bcdfb577e1c14c4635658c6b287d61d332d
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-08-10 03:28:14 +0530

    Time: 146 ms (9.84%), Space: 45.1 MB (80.71%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -2,30 +2,25 @@ class Solution {
     static final int MOD=1000000007;
     public int numOfArrays(int n, int m, int k) {
         int dp[][][]=new int[n+1][k+1][m+1];
-        for(int[][] a:dp){
-            for(int[] b:a){
-                Arrays.fill(b,-1);
+            for(int mx=0;mx<=m;mx++){
+                dp[n][k][mx]=1;
             }
-        }
-        int ans=solve(dp,0,0,0,n,m,k);
-        return ans;
-    }
-    int solve(int dp[][][],int idx,int sc,int mxsofar,int n,int m,int k){
-            if(idx>=n){
-                if(sc==k)return 1;
-                else return 0;
-            }
-            if(sc>k)return 0;
-            if(dp[idx][sc][mxsofar]!=-1)return dp[idx][sc][mxsofar];
-            int res=0;
-            for(int i=1;i<=m;i++){
-                if(i>mxsofar){
-                    res=(res+solve(dp,idx+1,sc+1,i,n,m,k))%MOD;
-                }else{
-                    res=(res+solve(dp,idx+1,sc,mxsofar,n,m,k))%MOD;
+        int ans=0;
+        for(int idx=n-1;idx>=0;idx--){
+            for(int sc=0;sc<=k;sc++){
+                for(int mxsofar=0;mxsofar<=m;mxsofar++){
+                    int res=0;
+                    for(int i=1;i<=m;i++){
+                        if(mxsofar<i){
+                            if(sc<k)res=(res+dp[idx+1][sc+1][i])%MOD;
+                        }else{
+                            res=(res+dp[idx+1][sc][mxsofar])%MOD;
+                        }
+                    }
+                    dp[idx][sc][mxsofar]=res;
                 }
             }
-            return dp[idx][sc][mxsofar]=res;
+        }
+        return dp[0][0][0];
     }
-
 }
