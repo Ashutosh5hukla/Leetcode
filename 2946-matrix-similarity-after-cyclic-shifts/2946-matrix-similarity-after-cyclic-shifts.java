9d5c9d89852a4c25ac047f8b842930e671e4b830
Ashutosh5hukla <ashutosh20051978@gmail.com>
2026-08-18 16:09:54 +0530

    Time: 1 ms (99.37%), Space: 46.9 MB (70.98%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -1,34 +1,17 @@
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
-        k=k%mat[0].length;
-        int temp[][]=new int[mat.length][mat[0].length];
+        int n=mat[0].length;
+        k=k%n;
         for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
-                temp[i][j]=mat[i][j];
-            }
-        }
-        while(k-->0){
-            for(int i=0;i<mat.length;i++){
-                if(i%2==0)s1(temp,i);
-                else s2(temp,i);
-            }
-        }
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
-                if(mat[i][j]!=temp[i][j])return false;
+            for(int j=0;j<n;j++){
+                if(i%2==0){
+                    if(mat[i][j]!=mat[i][(j+k)%n]) return false;
+                }
+                else{
+                    if(mat[i][j]!=mat[i][(j-k+n)%n]) return false;
+                }
             }
         }
         return true;
-    }
-    void s1(int arr[][],int idx){
-        int first=arr[idx][0];
-        for(int j=0;j<arr[idx].length-1;j++)arr[idx][j]=arr[idx][j+1];
-        arr[idx][arr[idx].length-1]=first;
-    }
-
-    void s2(int arr[][],int idx){
-        int last=arr[idx][arr[idx].length-1];
-        for(int j=arr[idx].length-1;j>0;j--)arr[idx][j]=arr[idx][j-1];
-        arr[idx][0]=last;
     }
 }
